096a6e03bd17a393d4196c5c3595077a825be24c
omar-faruk11 <omarabdullah1811@gmail.com>
2022-06-09 16:32:42 +0600

    simple Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home/Home";
+
+ export const context = createContext({})
 
 function App() {
+  const [light, setLigth] = useState(false)
   return (
-    <div className="App">
-      WELCOME TO OMAR FARUK PROTFOLIO
+    <context.Provider value={{light, setLigth}}>
+      <div data-theme={light? 'light': 'dark'} className='h-screen w-full'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
+    </context.Provider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,35 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui:{
+    themes: [{
+        light: {
+          "primary": "#4CA5FF",
+          "secondary": "#B374F8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#F5F9FF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+      {
+        dark: {
+          "primary": "#54A1FE",
+          "secondary": "#B475F8",
+          "accent": "#4B5563",
+          "neutral": "#0D1117",
+          "base-100": "#1F2937",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#F59E0B",
+          "error": "#FB7185",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
 }
